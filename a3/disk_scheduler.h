e833d913e66bf4a6adf0809cd1e4c71c31dd56c6
bravobvk <bravobvk@gmail.com>
2026-07-02 12:29:38 -0700

    Add constants for disk scheduling in disk_scheduler.h
    Updated header file to include constants for disk scheduling.

--- a3/disk_scheduler.h
+++ a3/disk_scheduler.h
@@ -1,11 +1,36 @@
 /*
- * disk_scheduler.h
- *
+CS 480 - Operating Systems [2026 Summer Session]
+Project: Assignment 3
+File: disk_scheduler.h
+
+Team Members:
+- Luis Villalon: cssc3169 
+- Bobby Bavongkhoun: cssc3110
+
+Description:
  * Common interface implemented by every disk arm scheduling
  * algorithm. New algorithms can be added by implementing this
  * interface and registering an instance in main() without touching
  * any existing algorithm code.
- */
+*/
+
+#ifndef SIMARM_CONSTANTS_H
+#define SIMARM_CONSTANTS_H
+
+namespace simarm {
+
+const int MIN_CYLINDER = 0;
+const int MAX_CYLINDER = 999;
+
+/* Fixed starting position of the disk arm for every algorithm, so
+ * that the algorithms can be compared fairly on identical
+ * conditions. */
+const int INITIAL_HEAD = 50;
+
+} // namespace simarm
+
+#endif // SIMARM_CONSTANTS_H
+
 
 #ifndef SIMARM_DISK_SCHEDULER_H
 #define SIMARM_DISK_SCHEDULER_H
